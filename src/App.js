efed240dc1a179e8336e875f934e5d26e1fab345
duchieu <duchieu.ctk41@gmail.com>
2020-12-01 22:03:17 +0700

    [1.0.0] showPerson & change color button.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Random from './Components/Random/Random';
 import Introduce from './Components/Introduce/Introduce';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={classes.App}>
         <Introduce />
       </div>
     );
--- src/Components/Introduce/Button/Button.js
+++ src/Components/Introduce/Button/Button.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import classes from './Button.css';
 
-const button = props => (
-    <button>Click me</button>
-);
+const button = props => {
+    let btnClasses = classes.Button;
+    if (props.showPerson) {
+        console.log("loz");
+        btnClasses = classes.Red;
+    }
+
+    return (
+        <button className={btnClasses} onClick={props.clicked}>Click me</button>
+    );
+}
 
 export default button;
