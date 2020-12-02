c47fc2b62c8f0040c5ee9298c6eacac552fb7993
duchieu <duchieu.ctk41@gmail.com>
2020-12-02 09:16:39 +0700

    [1.0.0] start burger.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Random from './Components/Random/Random';
 import Introduce from './Components/Introduce/Introduce';
 import classes from './App.css';
+import Layout from './hoc/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
-        <Introduce />
+        <Layout />
       </div>
     );
   }
---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Aux from '../Aux';
+
+class Layout extends Component {
+    render() {
+        return (
+            <Aux>
+                <p>dau cat moi</p>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
