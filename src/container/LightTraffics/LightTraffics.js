634acdc21bb5a98bcafe11eff13fc882dce9b906
duchieu <duchieu.ctk41@gmail.com>
2020-12-07 12:39:04 +0700

    [1.0.0] add traffic count down.

--- src/container/LightTraffics/LightTraffics.js
+++ src/container/LightTraffics/LightTraffics.js
@@ -5,15 +5,70 @@ import LightTraffic from './LightTraffic/LightTraffic';
 import classes from './LightTraffics.css';
 
 class LightTraffics extends Component {
-    render () {
-        return (
-            <div className={classes.Light}>
-                <LightTraffic typeColor="Green" />
-                <LightTraffic typeColor="Yellow" />
-                <LightTraffic typeColor="Red" />
-            </div>
-        );
+    
+  state={
+    red: "red", 
+    yellow: "yellow", 
+    green: "green",
+    next: "yellow"
+  }
+    setInterval ()  {
+        this._timeout = setTimeout(function () {
+        this.changeHandle();
+    }.bind(this), 500); 
     }
+
+        componentDidMount() {
+        this.setInterval(this.tick, 500);
+    }
+
+    componentWillUnmount()  {
+        clearInterval(this._timeout);
+    }
+
+
+    changeHandle() {
+        switch (this.state.next) {
+        case "red":
+                this.setState({
+            red:"red", 
+            yellow: "black", 
+            green: "black",
+            next: "yellow"
+        });
+        break;
+        case "yellow":
+        this.setState({
+            red:"black", 
+            yellow: "yellow", 
+            green: "black",
+            next: "green"
+        });
+        break;
+        case "green":
+        this.setState({
+            red:"black", 
+            yellow: "black", 
+            green: "green",
+            next: "red"
+        });
+        break;
+    }
+        
+            this._timeout = setTimeout(function () {
+            this.changeHandle();
+        }.bind(this), 5000);
+    }
+
+    render () {
+    return (
+    <div className={classes.Light}>
+            <LightTraffic styleColor={this.state.red} />
+            <LightTraffic styleColor={this.state.yellow} />
+            <LightTraffic styleColor={this.state.green} />
+    </div>
+    );
+}
 }
 
 export default LightTraffics;
