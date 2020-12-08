19b4e862b82e03591a373993fdcd0c835692ff0e
duchieu <duchieu.ctk41@gmail.com>
2020-12-08 11:01:16 +0700

    [1.0.0] add countDown traffic light.

--- src/Components/Light/Light.js
+++ src/Components/Light/Light.js
@@ -6,18 +6,22 @@ import classes from './Light.css';
 const light = props => {
     let divLight = null;
     let arrayClass = [classes.Light];
+    let timeLight = <CountDown inTime={props.inTime} />
     switch (props.nowColor) {
         case 'red':
             arrayClass = [classes.Light, classes.Red];
-            divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+            divLight = <div className={arrayClass.join(' ')}>{timeLight}</div>;
+            console.log("red");
             break;
         case 'green':
             arrayClass = [classes.Light, classes.Green];
-            divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+            divLight = <div className={arrayClass.join(' ')}>{timeLight}</div>;
+            console.log("green");
             break;
         case 'yellow':
                 arrayClass = [classes.Light, classes.Yellow];
-                divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+                divLight = <div className={arrayClass.join(' ')}>{timeLight}</div>;
+                console.log("yellow");
                 break;
         default:
             divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
--- src/container/TrafficLight/TrafficLight.js
+++ src/container/TrafficLight/TrafficLight.js
@@ -5,21 +5,113 @@ import classes from './TrafficLight.css';
 
 class TrafficLight extends Component {
     state={
-        typeColor : [
-            {red: "red", timeRed: 10},
-            {yellow: "yellow", timeYellow: 5},
-            {green: "green", timeGreen: 8}
-        ],
-        now: "red",
+        red: "",
+        green: "",
+        yellow: "",
+        time: 0,
+        now: "",
         next: "green"
     }
+    setInterval() {
+        this._timeout = setTimeout(() => {
+            this.changeHandler();
+        }, 500);
+    };
 
+    componentDidMount () {
+        this.setInterval(this.tick, 500);
+    }
+
+    componentWillMount () {
+        clearInterval(this._timeout);
+    }
+
+    changeHandler () {
+        switch (this.state.next) {
+            case 'red':
+                let timeCountDown = 10;
+                this.setState({
+                    red: "red",
+                    green: "",
+                    yellow: "",
+                    now: "red",
+                    time: timeCountDown,
+                    next: "green"
+                });
+                timeCountInterval = setInterval(() => {
+                    this.setState({
+                        time: --timeCountDown
+                    })
+                }, 1000);
+                this._timeout = setTimeout(function () {
+                    this.changeHandler();
+                    clearInterval(timeCountInterval);
+                }.bind(this), 10000);
+                break;
+                case 'green':
+                    timeCountDown = 8;
+                    this.setState({
+                        red: "",
+                        green: "green",
+                        yellow: "",
+                        now: "green",
+                        time: timeCountDown,
+                        next: "yellow"
+                    });
+                    let timeCountInterval = setInterval(() => {
+                        this.setState({
+                            time: --timeCountDown
+                        })
+                    }, 1000);
+                    this._timeout = setTimeout(function () {
+                        this.changeHandler();
+                        clearInterval(timeCountInterval);
+                    }.bind(this), 8000);
+                   
+                    break;
+                    case 'yellow':
+                        timeCountDown = 5;
+                        this.setState({
+                            red: "",
+                            green: "",
+                            yellow: "yellow",
+                            now: "yellow",
+                            time: timeCountDown,
+                            next: "red"
+                        });
+                        timeCountInterval = setInterval(() => {
+                            this.setState({
+                                time: --timeCountDown
+                            })
+                        }, 1000);
+                        this._timeout = setTimeout(function () {
+                            this.changeHandler();
+                            clearInterval(timeCountInterval);
+                        }.bind(this), 5000);
+                        break;          
+        }
+    }
     render () {
+
+        let arrayClass = [classes.TrafficLight];
+
+        switch(this.state.now) {
+            case 'red':
+                arrayClass = [classes.TrafficLight, classes.Blue];
+                break;
+            case 'green':
+                arrayClass = [classes.TrafficLight, classes.Violet];
+                break;
+            case 'yellow':
+                arrayClass = [classes.TrafficLight, classes.Salmon];
+                break;
+            
+        }
         return (
-            <div className={classes.TrafficLight}>
-                <Light nowColor={this.state.typeColor[0].red}/>
-                <Light nowColor={this.state.typeColor[1].green}/>
-                <Light nowColor={this.state.typeColor[2].yellow}/>
+            <div className={arrayClass.join(' ')}>
+                <Light nowColor={this.state.red} inTime={this.state.time}/>
+                <Light nowColor={this.state.green} inTime={this.state.time}/>
+                <Light nowColor={this.state.yellow} inTime={this.state.time}/>
             </div>
         );
     }
