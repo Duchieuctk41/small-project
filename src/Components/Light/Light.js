efb2dbfce2146a2ce728cbd1fb826ae3fce6cc8e
duchieu <duchieu.ctk41@gmail.com>
2020-12-08 10:13:31 +0700

    [1.0.0] adjust lightColor.

--- src/Components/Light/Light.js
+++ src/Components/Light/Light.js
@@ -3,10 +3,27 @@ import CountDown from './CountDown/CountDown';
 
 import classes from './Light.css';
 
-const light = (props) => (
-    <div className={classes.Light}>
-        <CountDown />
-    </div>
-);
+const light = props => {
+    let divLight = null;
+    let arrayClass = [classes.Light];
+    switch (props.nowColor) {
+        case 'red':
+            arrayClass = [classes.Light, classes.Red];
+            divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+            break;
+        case 'green':
+            arrayClass = [classes.Light, classes.Green];
+            divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+            break;
+        case 'yellow':
+                arrayClass = [classes.Light, classes.Yellow];
+                divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+                break;
+        default:
+            divLight = <div className={arrayClass.join(' ')}><CountDown /></div>;
+            break;
+    }
+    return divLight;
+}
 
 export default light;
--- src/container/TrafficLight/TrafficLight.js
+++ src/container/TrafficLight/TrafficLight.js
@@ -4,12 +4,22 @@ import Light from '../../Components/Light/Light';
 import classes from './TrafficLight.css';
 
 class TrafficLight extends Component {
+    state={
+        typeColor : [
+            {red: "red", timeRed: 10},
+            {yellow: "yellow", timeYellow: 5},
+            {green: "green", timeGreen: 8}
+        ],
+        now: "red",
+        next: "green"
+    }
+
     render () {
         return (
             <div className={classes.TrafficLight}>
-                <Light />
-                <Light />
-                <Light />
+                <Light nowColor={this.state.typeColor[0].red}/>
+                <Light nowColor={this.state.typeColor[1].green}/>
+                <Light nowColor={this.state.typeColor[2].yellow}/>
             </div>
         );
     }
