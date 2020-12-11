4f70077af719597689bcd95866a39669163c3961
duchieu <duchieu.ctk41@gmail.com>
2020-12-11 21:40:24 +0700

    [1.0.0] adjust function to const & traffic light.

--- src/container/TrafficLight/TrafficLight.js
+++ src/container/TrafficLight/TrafficLight.js
@@ -1,101 +1,101 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Light from '../../Components/Light/Light';
 
 import classes from './TrafficLight.css';
 
-class TrafficLight extends Component {
-    state={
+const trafficLight = () => {
+
+    const [clicks, setClicks] = useState({
         red: "",
         green: "",
         yellow: "",
         time: 0,
         now: "",
         next: "green"
-    }
-    setInterval() {
-        this._timeout = setTimeout(() => {
-            this.changeHandler();
-        }, 500);
-    };
+    });
 
-    componentDidMount () {
-        this.setInterval(this.tick, 500);
-    }
+    // setInterval() {
+    //     this._timeout = setTimeout(() => {
+    //         this.changeHandler();
+    //     }, 500);
+    // };
 
-    componentWillMount () {
-        clearInterval(this._timeout);
-    }
+    // componentDidMount () {
+    //     this.setInterval(this.tick, 500);
+    // }
 
-    changeHandler () {
-        switch (this.state.next) {
-            case 'red':
-                let timeCountDown = 10;
-                this.setState({
-                    red: "red",
-                    green: "",
-                    yellow: "",
-                    now: "red",
-                    time: timeCountDown,
-                    next: "green"
-                });
-                timeCountInterval = setInterval(() => {
-                    this.setState({
-                        time: --timeCountDown
-                    })
-                }, 1000);
-                this._timeout = setTimeout(function () {
-                    this.changeHandler();
-                    clearInterval(timeCountInterval);
-                }.bind(this), 10000);
-                break;
-                case 'green':
-                    timeCountDown = 8;
-                    this.setState({
-                        red: "",
-                        green: "green",
-                        yellow: "",
-                        now: "green",
-                        time: timeCountDown,
-                        next: "yellow"
-                    });
-                    let timeCountInterval = setInterval(() => {
-                        this.setState({
-                            time: --timeCountDown
-                        })
-                    }, 1000);
-                    this._timeout = setTimeout(function () {
-                        this.changeHandler();
-                        clearInterval(timeCountInterval);
-                    }.bind(this), 8000);
+    // componentWillMount () {
+    //     clearInterval(this._timeout);
+    // }
+
+    // const changeHandler () {
+    //     switch (this.state.next) {
+    //         case 'red':
+    //             let timeCountDown = 10;
+    //             this.setState({
+    //                 red: "red",
+    //                 green: "",
+    //                 yellow: "",
+    //                 now: "red",
+    //                 time: timeCountDown,
+    //                 next: "green"
+    //             });
+    //             timeCountInterval = setInterval(() => {
+    //                 this.setState({
+    //                     time: --timeCountDown
+    //                 })
+    //             }, 1000);
+    //             this._timeout = setTimeout(function () {
+    //                 this.changeHandler();
+    //                 clearInterval(timeCountInterval);
+    //             }.bind(this), 10000);
+    //             break;
+    //             case 'green':
+    //                 timeCountDown = 8;
+    //                 this.setState({
+    //                     red: "",
+    //                     green: "green",
+    //                     yellow: "",
+    //                     now: "green",
+    //                     time: timeCountDown,
+    //                     next: "yellow"
+    //                 });
+    //                 let timeCountInterval = setInterval(() => {
+    //                     this.setState({
+    //                         time: --timeCountDown
+    //                     })
+    //                 }, 1000);
+    //                 this._timeout = setTimeout(function () {
+    //                     this.changeHandler();
+    //                     clearInterval(timeCountInterval);
+    //                 }.bind(this), 8000);
                    
-                    break;
-                    case 'yellow':
-                        timeCountDown = 5;
-                        this.setState({
-                            red: "",
-                            green: "",
-                            yellow: "yellow",
-                            now: "yellow",
-                            time: timeCountDown,
-                            next: "red"
-                        });
-                        timeCountInterval = setInterval(() => {
-                            this.setState({
-                                time: --timeCountDown
-                            })
-                        }, 1000);
-                        this._timeout = setTimeout(function () {
-                            this.changeHandler();
-                            clearInterval(timeCountInterval);
-                        }.bind(this), 5000);
-                        break;          
-        }
-    }
-    render () {
-
+    //                 break;
+    //                 case 'yellow':
+    //                     timeCountDown = 5;
+    //                     this.setState({
+    //                         red: "",
+    //                         green: "",
+    //                         yellow: "yellow",
+    //                         now: "yellow",
+    //                         time: timeCountDown,
+    //                         next: "red"
+    //                     });
+    //                     timeCountInterval = setInterval(() => {
+    //                         this.setState({
+    //                             time: --timeCountDown
+    //                         })
+    //                     }, 1000);
+    //                     this._timeout = setTimeout(function () {
+    //                         this.changeHandler();
+    //                         clearInterval(timeCountInterval);
+    //                     }.bind(this), 5000);
+    //                     break;          
+    //     }
+    // }
         let arrayClass = [classes.TrafficLight];
 
-        switch(this.state.now) {
+        switch(clicks.now) {
             case 'red':
                 arrayClass = [classes.TrafficLight, classes.Blue];
                 break;
@@ -109,12 +109,11 @@ class TrafficLight extends Component {
         }
         return (
             <div className={arrayClass.join(' ')}>
-                <Light nowColor={this.state.red} inTime={this.state.time}/>
-                <Light nowColor={this.state.green} inTime={this.state.time}/>
-                <Light nowColor={this.state.yellow} inTime={this.state.time}/>
+                <Light nowColor={clicks.red} inTime={clicks.time}/>
+                <Light nowColor={clicks.green} inTime={clicks.time}/>
+                <Light nowColor={clicks.yellow} inTime={clicks.time}/>
             </div>
         );
-    }
 }
 
-export default TrafficLight;
+export default trafficLight;
