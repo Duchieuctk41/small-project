5f93cf13a2bb6dce6a4340c0dccca81b65e33411
duchieu <duchieu.ctk41@gmail.com>
2020-12-13 12:07:33 +0700

    [1.0.0] add restaurant

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import Aux from './hoc/Aux';
-import Layout from './hoc/Layout/Layout';
-import Burger from './container/BurgerBuilder/BurgerBuilder';
+import Restaurant from './Components/Restaurant/Restaurant';
+
 import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
       <Aux className={classes.App}>
-       <Layout>
-          <Burger />
-       </Layout>
+       <Restaurant />
       </Aux>
     );
   }
